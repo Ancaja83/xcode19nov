6d40fde0a3ccdad8d673fe299a78c2ab42bb54fb
Ancaja <236369786+Ancaja83@users.noreply.github.com>
2025-11-19 09:13:14 +0100

    Initial Commit

---
+++ xcode19nov/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  xcode19nov
+//
+//  Created by Catharina Jaunviksna on 2025-11-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xcode19nov/xcode19novApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcode19novApp.swift
+//  xcode19nov
+//
+//  Created by Catharina Jaunviksna on 2025-11-19.
+//
+
+import SwiftUI
+
+@main
+struct xcode19novApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
